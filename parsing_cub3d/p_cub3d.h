546796cf8074ac6a82a796a944a703254ad1fab8
Hamza Aitaabella <haitaabe@e1r6p11.1337.ma>
2025-10-10 09:09:31 +0100

    test

--- parsing_cub3d/p_cub3d.h
+++ parsing_cub3d/p_cub3d.h
@@ -6,48 +6,13 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 15:14:52 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/10/06 16:54:37 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/10/08 17:09:45 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_CUB3D_H
 #define PARSING_CUB3D_H
 
-
-typedef struct s_config
-{
-    char    *no_texture;
-    char    *so_texture;
-    char    *we_texture;
-    char    *ea_texture;
-    int     floor_color[3];
-    int     ceiling_color[3];
-}   t_config;
-
-typedef struct s_map
-{
-    char    **map;
-    int     width;
-    int     height;
-    float  player_x;
-    float  player_y;
-    char    player_dir;
-}   t_map;
-
-typedef struct s_data
-{
-    t_config config;
-    t_map map;
-}   t_data;
-
-
-typedef struct s_file
-{
-    char **config_lines; 
-    char **map_lines;
-    int   direction_count;
-    int   raw_map_started;
-}   t_file;
 
 
 

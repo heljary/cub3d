6e45054241a6c5248abf7a1d1ab9ea6d9aed23f1
Hamza Aitaabella <haitaabe@e1r13p9.1337.ma>
2025-10-14 09:01:58 +0100

    set up the header file to start doing the work

--- includes/p_cub3d.h
+++ includes/p_cub3d.h
@@ -6,14 +6,16 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 15:14:52 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/10/13 19:11:58 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/10/14 08:37:02 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_CUB3D_H
 #define PARSING_CUB3D_H
 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 typedef struct s_textures 
 {
@@ -28,7 +30,7 @@ typedef struct s_colors
 {
     int floor[3];
     int ceiling[3];
-} s_colors;
+} t_colors;
 
 
 typedef struct s_map
@@ -41,12 +43,36 @@ typedef struct s_map
 typedef struct s_data
 {
     t_textures textures;
-    s_colors colors;
+    t_colors colors;
     t_map map;
-} s_data;
+} t_data;
 
 
+int parse_cub_file(const char *path, t_data *data);
 
 
+// these functions for textures and colors
+int parse_config_line(char *line, t_data *data);
+int parse_textures(char *id, char *value, t_textures *tx);
+int parse_color(char id, char *value, t_colors *colors);
+
+
+// reading map
+char **collect_map_lines(int fd, size_t *out_count);
+
+int build_map(char **lines, size_t count, t_map *map);
+
+
+// to validate our data 
+int validate_data(const t_data *data);
+int validate_map_chars(const t_map *map);
+int validate_map_enclosed(const t_map *map);
+int find_player_pos(const t_map *map, int *px, int *py, char *dir);
+
+
+// to free our data (avoiding leaks and other issues)
+void free_map(char **map, int height);
+void free_data(t_data *data);
+void exit_error(const char *msg, t_data *data, int code);
 
 #endif

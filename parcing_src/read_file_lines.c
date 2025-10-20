33de99141630fe0e04732f2d4e6932817ada6c3e
Hamza Aitaabella <haitaabe@e1r1p6.1337.ma>
2025-10-20 13:26:44 +0100

    adding the important stuff

--- parcing_src/read_file_lines.c
+++ parcing_src/read_file_lines.c
@@ -6,7 +6,7 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 11:40:33 by haitaabe          #+#    #+#             */
-/*   Updated: 2025/10/20 11:59:36 by haitaabe         ###   ########.fr       */
+/*   Updated: 2025/10/20 13:26:20 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,5 @@
 
 char **read_file_lines(const char *filename)
 {
-    int fd;
-    int open_file = open("../maps/map1.cub", O_RDONLY);
-    if (open_file == -1)
-        return (NULL);
-    char buffer[1024];
     
 }
